79fc603867eb56e7ef1ad993859289aa8a69ae8a
Cauã Pablo <cauapxd1@gmail.com>
2022-05-15 17:58:24 -0300

    atualizações

---
+++ src/src/Principal/Main.java
@@ -0,0 +1,14 @@
+package src.Principal;
+import view.LoginView;
+public class Main {
+    public static void main(String[] args) {
+
+        //INSTANCIAÇÃO DO LOGIN;
+        LoginView user1 = new LoginView();
+        user1.inicio();
+        //teste
+        //seundo teste de clone com o intelijj
+
+
+    }
+}
